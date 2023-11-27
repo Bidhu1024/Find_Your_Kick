f207c37f45c57dbd7ba5aa568d8ac37bca007609
Bidhu1024 <bidhu1024av@gmail.com>
2023-11-27 08:59:29 +0530

    start

---
+++ src/Components/Screens/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box,Stack } from '@mui/material'
+import logo from "../../Assets/logo1.png"
+const Login = () => {
+  return (
+    <Stack sx={{width:"100vw", position:"relative", height:"100vh",backgroundColor:"red", justifyContent:"center", alignItems:"center"}}>
+{/* <img src={logo} alt="shoe" style={{position:"absolute",top:0,padding:"1rem", left:0, width:"6rem", height:"6rem"}} /> */}
+<Box sx={{width:"25rem", height:"30rem", bgcolor:"white"}}>
+
+</Box>
+    </Stack>
+  )
+}
+
+export default Login
