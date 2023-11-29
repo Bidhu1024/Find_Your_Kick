13a395a631ff743cef75bb7e8022b21a2014317d
Bidhu1024 <bidhu1024av@gmail.com>
2023-11-29 23:52:12 +0530

    fix

--- src/Components/Screens/Login/Login.jsx
+++ src/Components/Screens/Login/Login.jsx
@@ -11,18 +11,18 @@ const Login = () => {
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
   const nav = useNavigate()
-  const handleLogin = (e)=>{
+  const handleLogin = (e) => {
     e.preventDefault()
     dispatch(setUser({ email, password, name }));
     nav("/dashboard")
   }
   return (
-    <Stack sx={{ width: "100vw", position: "relative", height: "100vh", justifyContent: "center", alignItems: "center", backgroundImage: `url(${logo1})`,backgroundRepeat:"no-repeat",backgroundSize:"cover" }}>
+    <Stack sx={{ width: "100vw", position: "relative", height: "100vh", justifyContent: "center", alignItems: "center", backgroundImage: `url(${logo1})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
       <Box sx={{ borderRadius: "1rem", width: "35rem", height: "35rem", padding: "2rem", bgcolor: "white", boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px" }}>
         <form autoComplete='off'>
           <Box display="flex" alignItems={"center"} gap=".5rem">
 
-            <img src={logo} alt="" style={{ width: "2.5rem", height: "2.5rem" }} />
+            <img  src={logo} alt="" style={{ width: "2.5rem", height: "2.5rem" }} />
 
             <Typography fontSize={"1.2rem"} color="purple" fontWeight={"600"} fontFamily={"Arial, Helvetica, sans-serif"}>
               Get Your Kicks
@@ -38,7 +38,7 @@ const Login = () => {
 
           </Stack>
           <Stack sx={{ mt: "1rem" }}>
-          <Typography fontSize={"1rem"} fontWeight={"600"} sx={{ mt: ".3rem" }}>
+            <Typography fontSize={"1rem"} fontWeight={"600"} sx={{ mt: ".3rem" }}>
               Name
             </Typography>
             <TextField value={name} onChange={(e) => setName(e.target.value.toLowerCase())} size="small" type='text' />
